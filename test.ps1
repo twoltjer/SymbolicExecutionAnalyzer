344c725fbf9506657fa2fa1e24bee4557491d412
Thomas Woltjer <thomaswoltjer@gmail.com>
2022-05-01 00:40:13 -0400

    Add test script

--- test.ps1
+++ test.ps1
@@ -5,10 +5,11 @@ $configuration = 'Release'
 $artifactsDir = Join-Path $PSScriptRoot 'artifacts'
 
 $msbuild = Join-Path $visualStudioInstallation 'MSBuild\Current\Bin\MSBuild.exe'
-# Build solution
+# Build and test solution
 & $msbuild /t:Build /restore /v:minimal /p:Configuration=$configuration
-# Package
-& $msbuild 'SymbolicExecution\SymbolicExecution.Package' /t:Pack /p:NoBuild=true /p:PackageOutputPath="$artifactsDir" /v:minimal /p:Configuration=$configuration
-
-# Copy extension to artifacts
-Copy-Item SymbolicExecution\SymbolicExecution.Vsix\bin\$configuration\SymbolicExecution.vsix $artifactsDir
+dotnet test --no-build --configuration $configuration
+# Ensure a debug build is made for tests
+if ($configuration -ne 'Debug') {
+    & $msbuild /t:Build /restore /v:minimal /p:Configuration='Debug'
+    dotnet test --no-build --configuration Debug
+}
