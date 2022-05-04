b5a66029063b7ec6fdc855adadd47f90ef41ac0e
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-05-04 12:45:13 -0400

    Attempt to fix coverage analysis by adding path

--- coverage.ps1
+++ coverage.ps1
@@ -1,4 +1,5 @@
 $ocpath = Join-Path $HOME .nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe
 & $ocpath -register:user -target:"dotnet.exe" -targetargs:"test --no-build --configuration Debug" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
+$env:PATH = 'C:\msys64\usr\bin;' + $env:PATH
 Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
 bash codecov.sh -f "coverage.xml" -t 992788fa-0d03-4f07-8b6d-2ae6fde6c6cd
