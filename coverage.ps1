3bf925b4c222f7a1f2e1f8e9fded8428358d08b3
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-05-05 13:17:09 -0400

    Use exe instead of bash script for updating

--- coverage.ps1
+++ coverage.ps1
@@ -1,4 +1,5 @@
 $ocpath = Join-Path $HOME .nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe
 & $ocpath -register:user -target:"dotnet.exe" -targetargs:"test --no-build --configuration Debug" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
-Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile codecov.sh
-bash codecov.sh -f "coverage.xml"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -OutFile codecov.sh
+.\codecov.sh -f "coverage.xml"
