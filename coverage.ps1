c053a78bbedefc4b6cfbe3fcbdd5e1a220599081
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-05-05 13:35:32 -0400

    Coverage in github

--- coverage.ps1
+++ coverage.ps1
@@ -1,5 +1,5 @@
 $ocpath = Join-Path $HOME .nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe
-& $ocpath -register:user -target:"dotnet.exe" -targetargs:"test --no-build --configuration Debug" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
+& $ocpath -register:user -target:"dotnet.exe" -targetargs:"test --no-build" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -OutFile codecov.sh
-.\codecov.sh -f "coverage.xml"
+Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -OutFile codecov.exe
+.\codecov.exe -f "coverage.xml"
