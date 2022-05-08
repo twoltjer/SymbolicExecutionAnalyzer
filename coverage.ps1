0b4cb80333c71295391ecb660010d0f94a8f9aad
Thomas Woltjer <Thomas.Woltjer@VibrationResearch.com>
2022-05-08 19:31:33 -0400

    Don't register opencover as user

--- coverage.ps1
+++ coverage.ps1
@@ -1,5 +1,5 @@
 $ocpath = Join-Path $HOME .nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe
-& $ocpath -register:user -target:"dotnet.exe" -targetargs:"test --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=opencover" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
+& $ocpath -register -target:"dotnet.exe" -targetargs:"test --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=opencover" -output:".\coverage.xml" -filter:"+[SymbolicExecution*]* -[SymbolicExecution.Test*]*"
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -OutFile codecov.exe
 .\codecov.exe -f "coverage.xml"
