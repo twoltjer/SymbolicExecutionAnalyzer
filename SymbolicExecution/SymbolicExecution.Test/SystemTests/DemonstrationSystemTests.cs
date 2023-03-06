0352a0a3651f1aee78b0f29802ab602893026d99
Thomas Woltjer <thomas.woltjer@vibrationresearch.com>
2023-03-06 13:44:00 -0500

    Add demonstration system test

---
+++ SymbolicExecution/SymbolicExecution.Test/SystemTests/DemonstrationSystemTests.cs
@@ -0,0 +1,48 @@
+namespace SymbolicExecution.Test.SystemTests;
+
+public class DemonstrationSystemTests
+{
+    [Fact]
+    [Trait("Category", "System")]
+    public async Task TestPascalsTriangleRowPrinting()
+    {
+        var source = @$"using System;
+using {typeof(SymbolicallyAnalyzeAttribute).Namespace};
+
+class TestClass
+{{
+	[SymbolicallyAnalyze]
+	void PrintRowTen()
+	{{
+		PrintRow(10);
+	}}
+
+	[SymbolicallyAnalyze]
+	void PrintRowTenThousand()
+	{{
+		PrintRow(10000);
+	}}
+
+	void PrintRow(int n)
+	{{
+		
+	}}
+
+	int[] GetRow(int n)
+	{{
+		var row = new int[n + 1];
+		var previousRow = GetRow(n - 1);
+		row[0] = 1;
+		row[n] = 1;
+		for (int i = 1; i < n; i++)
+		{{
+			row[i] = previousRow[i - 1] + previousRow[i];
+		}}
+		return row;
+	}}
+}}
+";
+
+        await VerifyCS.VerifyAnalyzerAsync(source);
+    }
+}
